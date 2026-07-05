6c9da97a92bb19f670b47b196be6d620f3ecf8a2
Pradeesh25062002 <pradeesh2002@gmail.com>
2026-07-05 22:04:15 +0530

    Reduced memory usage in RAG

--- RAG.py
+++ RAG.py
@@ -3,21 +3,29 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 DB_PATH = "chroma_db"
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+db = None
 
-db = Chroma(
-    persist_directory=DB_PATH,
-    embedding_function=embeddings
-)
+def get_db():
+    global db
+
+    if db is None:
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+        db = Chroma(
+            persist_directory=DB_PATH,
+            embedding_function=embeddings
+        )
+
+    return db
+
 
 def retrieve_policy(query):
-
+    db = get_db()
     docs = db.similarity_search(query, k=3)
 
     text = ""
-
     for doc in docs:
         text += doc.page_content + "\n"
 
